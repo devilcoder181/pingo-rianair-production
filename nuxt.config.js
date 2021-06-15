3ac68f01246036b0e2bbb83977a877977f7cfa9d
Kiran Dennis <kiran@pgsuae.com>
2021-06-15 10:19:24 +0400

    heroku to production

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,4 @@
-const isDev = process.env.NODE_ENV === 'development'
+const isDev = process.env.NODE_ENV === 'production'
 const useEmulators = false // manually change if emulators needed
 
 export default {
@@ -96,7 +96,7 @@ export default {
       importScripts: ['/firebase-auth-sw.js'],
       // by default the workbox module will not install the service worker in dev environment to avoid conflicts with HMR
       // only set this true for testing and remember to always clear your browser cache in development
-      dev: process.env.NODE_ENV === 'development',
+      dev: process.env.NODE_ENV === 'production',
     },
   },
 
